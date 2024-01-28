11608c9bf964e953f29641c3e6e20f4ecb77f511
Aliaksandr Sinevich <sinevichaleksandr@gmail.com>
2024-01-28 18:03:00 +0300

    3rd mode/3rd task - trim_symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,24 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (typeof size === 'undefined') {
+    return string;
+  }
+  if (!size || !string) {
+    return '';
+  }
+  let cont = 1;
+  let symb = string[0];
+  let result = string[0];
+  for (let i = 0; i < string.length; i++) {
+    if (symb == string[i] && cont < size) {
+      result += string[i];
+      cont++;
+    } else if (symb != string[i]) {
+      symb = string[i];
+      result += string[i];
+      cont = 1;
+    }
+  }
+  return result;
 }
